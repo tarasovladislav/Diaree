d6c647032e1561424b2473dccadc8580653c6487
Aaisha Rehman <aashu1471@gmail.com>
2023-11-16 16:59:40 +0100

    sample tests

--- server/tests/server.test.ts
+++ server/tests/server.test.ts
@@ -1,11 +1,30 @@
 import request from 'supertest';
-import app from '../index'; // Import your server app
+import app from '../index';
+import Diary from '../models/diary'
 
-describe('Server Tests', () => {
-  it('should return "Hello, World!"', async () => {
-    const response = await request(app).get('/');
+describe('GET / - getAllDiaryEntries', () => {
+    it('should return all diary entries', async () => {
+        const sampleData = [
+            {
+                "title": "Sample Diary Entry",
+                "text": "This is the content of the diary entry. It can be a longer piece of text.",
+                "date": "2023-11-16",
+                "createdAt": "2023-11-16T01:00:00.000Z",
+                "imageUrl": "https://example.com/image.jpg",
+                "tags": ["personal", "reflection"]
+            }
+        ];
+        await Diary.insertMany(sampleData);
+        const response = await request(app).get('/diary/entries');
 
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, World!');
-  });
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(sampleData);
+    });
+
+    it('should handle errors and return a 500 status', async () => {
+        jest.spyOn(Diary, 'find').mockRejectedValueOnce(new Error('Some error'));
+        const response = await request(app).get('/diary/entries');
+        expect(response.status).toBe(500);
+        expect(response.body).toEqual({ error: 'Internal server error' });
+    });
 });
