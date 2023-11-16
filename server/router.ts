15326c14ebc8de0acdb0410db04d0508a4b34a4b
erik <erik@lenzmann.net>
2023-11-16 13:10:02 +0100

    refactored router

---
+++ server/router.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+const router = Router();
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
+import diaryController from './controllers/diaryController';
+import tagController from './controllers/tagController';
+
+//TODO: Change controller names
+
+//Diary
+router.get('/diary/entries', diaryController.getAllDiaryEntries);
+//router.get('/diary/entries/:id', diaryController.getDiaryEntryById); //getOneDiaryEntry
+//router.post('/diary/entries', diaryController.postDiaryEntry); //addDiaryEntry
+//router.put('/diary/entries/:id', diaryController.putDiaryEntrie); //INFO: edits the diary entry
+router.delete('/diary/entries/:id', diaryController.deleteDiaryEntry);
+router.get('/diary/entries/recent', diaryController.getRecentDiaryEntries);
+//Tags
+router.get('/diary/tags', tagController.getAllTags);
+//router.post('/diary/tags', tagController.postTag); //addTag
+router.delete('/diary/tags/:id', tagController.deleteTag);
+//Date
+router.get('/diary/entries/:date', diaryController.getDiaryEntryByDate);
+//Image
+router.post('/diary/image/upload', upload.single('image'), diaryController.uploadImage);
+
+//TODO: Add user => router.post('/user/login', ...)
+
+module.exports = router;
