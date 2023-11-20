8fdaa719a932591f69fb76e0359bcf62e0b3e694
erik <erik@lenzmann.net>
2023-11-20 13:42:48 +0100

    save 2

--- client/src/new/Navbar/Account/Account.tsx
+++ client/src/new/Navbar/Account/Account.tsx
@@ -6,6 +6,11 @@ import { useAuth } from '../../../Utils/auth';
 const Account: React.FC<{ isDashboardOpen: boolean, setIsDashboardOpen: (isDashboardOpen: boolean) => void }> = ({ isDashboardOpen, setIsDashboardOpen }) => {
     const { logout } = useAuth();
 
+    const handleLogout = () => {
+        const shouldReset = window.confirm('Are you sure you want to log out?');
+        if (shouldReset) logout();
+    }
+
     return (
         <div className="Account" id={isDashboardOpen ? 'fadeIn' : 'fadeOut'}>
             <div className="Account-Main">
@@ -14,13 +19,13 @@ const Account: React.FC<{ isDashboardOpen: boolean, setIsDashboardOpen: (isDashb
                         <h2>Account</h2>
                     </div>
                     <div className="End">
-                        <img src={DiaryLogo} alt="Logo" style={{ height: '32px' }} />
-                        <img src={User} alt="User" style={{ height: '24px', borderRadius: '0' }} onClick={() => { setIsDashboardOpen(!isDashboardOpen) }} />
+                        <img src={DiaryLogo} alt="Logo" style={{ height: '32px' }} onClick={() => { setIsDashboardOpen(!isDashboardOpen) }} />
+                        <img src={User} alt="User" style={{ height: '24px', borderRadius: '0' }} />
                     </div>
                 </div>
                 <footer>
                     <button>Reset Diary</button>
-                    <button onClick={() => { logout() }}>Log out</button>
+                    <button onClick={handleLogout}>Log out</button>
                 </footer>
             </div>
         </div>
