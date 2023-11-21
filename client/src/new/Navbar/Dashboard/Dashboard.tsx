72457a10ad1959d253a0d0e636588e75d5438c6c
Aaisha Rehman <aashu1471@gmail.com>
2023-11-21 11:55:23 +0100

    random memory is working

--- client/src/new/Navbar/Dashboard/Dashboard.tsx
+++ client/src/new/Navbar/Dashboard/Dashboard.tsx
@@ -5,15 +5,31 @@ import DiaryLogo from '../../../assets/diary.png';
 import User from '../../../assets/user.png';
 import { useEffect, useState } from 'react';
 import { useDiary } from '../../../Utils/diary';
+import { useSingleEntry } from '../../../Utils/singleEntry';
 
 const Dashboard: React.FC<{ isDashboardOpen: boolean, setIsDashboardOpen: (isDashboardOpen: boolean) => void }> = ({ isDashboardOpen, setIsDashboardOpen }) => {
     const { diaries, tagList } = useDiary();
+    const { setSelectedEntry, selectedEntry, setIsShowSingleEvent } = useSingleEntry();
     const [recentEvents, setRecentEvents] = useState([]);
 
     useEffect(() => {
         const recents = diaries.sort((a, b) => b._id - a._id)
         setRecentEvents(recents)
     }, [diaries])
+
+    const handleRandomMemoryClick = () => {
+        if (diaries.length) {
+            const randomIndex = Math.floor(Math.random() * diaries.length);
+            const randomMemory = diaries[randomIndex];
+            setSelectedEntry(randomMemory);
+            setIsShowSingleEvent(true);
+        } else {
+            // Handle the case where there are no diary entries
+            setSelectedEntry({title: "Random Memory", text: "You have no diary entries.", imageURL: ""})
+            setIsShowSingleEvent(true);
+            console.log('You have no diary entries.');
+        }
+    }
 
     return (
         <div className="Dashboard" id={isDashboardOpen ? 'fadeIn' : 'fadeOut'}>
@@ -33,7 +49,7 @@ const Dashboard: React.FC<{ isDashboardOpen: boolean, setIsDashboardOpen: (isDas
                 </div>
                 <div className="Border"></div>
                 <div className="Random-Memorie">
-                    <button>Random memory</button>
+                    <button onClick={() => { handleRandomMemoryClick() }}>Random memory</button>
                 </div>
                 <div className="Border" style={{ paddingBlockStart: '0px' }}></div>
                 <div className="Tags">
