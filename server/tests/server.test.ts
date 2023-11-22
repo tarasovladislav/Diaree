ed2b1d07ca662efb2aab6d783e5f5bd5986f0e4e
Aaisha Rehman <aashu1471@gmail.com>
2023-11-22 14:44:13 +0100

    user tests are now working

--- server/tests/server.test.ts
+++ server/tests/server.test.ts
@@ -1,97 +1,73 @@
 import request from 'supertest';
-import router from '../router.js';
-import cors from 'cors';
-import express from 'express';
 import mongoose from "mongoose";
 import User from '../models/user.js';
 import { tokenToUserId } from '../utils/userUtils.js';
-
-
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/diary';
-
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router);
+import startServer from '../index.js';
+import { Server } from 'http';
 
 
 describe('/user/account endpoint', () => {
 
-    // Establish MongoDB connection before running the tests
+    let server: Server;
+
+    // Open MongoDB connection and start the server
     beforeAll(async () => {
-        await mongoose.connect(MONGO_URI);
+        server = await startServer();
+        await User.findOneAndDelete({ username: 'TEST' });
+        console.log('opening')
     });
 
-    // Close the MongoDB connection after all tests are done
+    // Close MongoDB connection and stop the server
     afterAll(async () => {
+        await User.findOneAndDelete({ username: 'TEST' });
         await mongoose.connection.close();
+        server.close();
+        console.log('closing')
     });
 
     const mockUser = { username: 'TEST', password: 'TEST' };
 
-
-    it('should be able to create a user', async () => {
-        await User.findOneAndDelete({ username: 'TEST' });
-        const response = await request(app)
+    it('should be able to create a user and get a token', async () => {
+        const response = await request(server)
             .post('/user/account/register')
             .set('Content-Type', 'application/json')
             .send(mockUser);
+
+        const data = await request(server)
+            .get('/user/account')
+            .set('Authorization', `${response.body.token}`)
 
         expect(response.status).toBe(201);
         expect(response.body.token).toBeTruthy();
+        expect(data.status).toBe(200);
+        expect(data.body).toBeTruthy();
 
-        const user_id = tokenToUserId(response.body.token);
-
-        await User.findOneAndDelete({ user_id });
     });
 
-    it('should be able to log in', async () => {
-        await User.findOneAndDelete({ username: 'TEST' });
-        const user = await request(app)
-            .post('/user/account/register')
-            .set('Content-Type', 'application/json')
-            .send(mockUser);
-
-        const login = await request(app)
+    it('should be able to log in ', async () => {
+        const login = await request(server)
             .post('/user/account/login')
             .set('Content-Type', 'application/json')
             .send(mockUser);
 
-        expect(user.status).toBe(201);
-        expect(user.body.token).toBeTruthy();
         expect(login.status).toBe(200);
         expect(login.body.token).toBeTruthy();
-
-        const user_id = tokenToUserId(login.body.token);
-
-        await User.findOneAndDelete({ user_id });
     });
 
-    it('should be able to get user data', async () => {
-        await User.findOneAndDelete({ username: 'TEST' });
-        const user = await request(app)
-            .post('/user/account/register')
-            .set('Content-Type', 'application/json')
-            .send(mockUser);
+    // it('should be able to get user data', async () => {
+    //     const user = await request(server)
+    //         .post('/user/account/register')
+    //         .set('Content-Type', 'application/json')
+    //         .send(mockUser);
 
-        expect(user.status).toBe(201);
-        expect(user.body.token).toBeTruthy();
+    //     expect(user.status).toBe(201);
+    //     expect(user.body.token).toBeTruthy();
 
-        const data = await request(app)
-            .get('/user/account')
-            .set('Authorization', `${user.body.token}`)
+    //     const data = await request(server)
+    //         .get('/user/account')
+    //         .set('Authorization', `${user.body.token}`)
 
-        expect(data.status).toBe(200);
-        expect(data.body).toBeTruthy();
-
-        const user_id = tokenToUserId(user.body.token);
-
-        await User.findOneAndDelete({ user_id });
-    });
+    //     expect(data.status).toBe(200);
+    //     expect(data.body).toBeTruthy();
+    // });
 });
-
-// afterAll(async () => {
-//     await mongoose.connection.close();
-// })
